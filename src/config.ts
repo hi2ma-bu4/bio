54e2a022e67f381a4f5708ad4378f05fc841c11e
hi2ma-bu4 <hi2ma.bu4.bot@gmail.com>
2025-11-22 16:42:27 +0900

    ✨alt表示追加

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,5 @@ export const NAV_LINKS = [
 	{ href: `${BASE_DIR}works/`, text: "Works" },
 	{ href: `${BASE_DIR}link/`, text: "Link" },
 ] as const;
+
+export const CLASS_AUTO_IMG_ALT = "auto-img-alt";
