f765e9f73dc6a1722e5b47f185a1f9bc74c67119
hi2ma-bu4 <hi2ma.bu4.bot@gmail.com>
2026-05-10 23:12:36 +0900

    🐛 フォーマット規則に合わせて修正 fix: update JSDoc comment for createUrl function to use correct @returns tag

--- src/scripts/libs/url-utils.ts
+++ src/scripts/libs/url-utils.ts
@@ -4,7 +4,7 @@ import { SITE_URL } from "../../config";
  * 末端スラッシュの付与を行うURL生成関数
  * ※クエリパラメータとハッシュタグが削除されます
  * @param pathname - 生成するURLのパス名
- * @return 生成されたURL
+ * @returns 生成されたURL
  */
 export function createUrl(pathname: string): string {
 	const url = new URL(pathname, SITE_URL);
