326990783543b3f83a0f1ab5eb363f2ccff9c050
hi2ma-bu4 <hi2ma.bu4.bot@gmail.com>
2026-05-10 18:07:03 +0900

    🏗️ ページ構造を分割 feat: add Logo and NavLinks components; create works data structure
    - Introduced Logo component for site branding with configurable id and class.
    - Added NavLinks component to dynamically render navigation links based on configuration.
    - Created works data structure to store information about various projects, including titles, descriptions, and links.

---
+++ dist/assets/js/chunks/Layout-DcCVXUd6.js
@@ -0,0 +1,83 @@
+import { a as createComponent, m as maybeRenderHead, f as addAttribute, ez as createTransitionScope, d as renderComponent, eA as $$Bars3, eu as $$XMark, b as renderScript, r as renderTemplate, c as createAstro, e as renderSlot, g as renderHead, $ as $$GoogleFontsOptimizer, eB as $$ClientRouter, h as $$SEO } from './vendor-zsY7UJHi.js';
+import 'piccolore';
+import { c as $$ThemeToggle, d as $$NavLinks, e as $$Logo, B as BASE_DIR, A as AUTHOR, S as SITE_TITLE, a as SITE_DESCRIPTION, $ as $$Footer, b as $$Header, G as GTAG_ID } from './Header-Bnk7kFAb.js';
+/* empty css               */
+
+const pwaAssetsHead = {"links":[{"href":"/bio/favicon.ico","rel":"icon","sizes":"48x48"},{"href":"/bio/favicon.svg","rel":"icon","sizes":"any","type":"image/svg+xml"},{"href":"/bio/apple-touch-icon-180x180.png","rel":"apple-touch-icon"}],"themeColor":{"content":"#1D396F"}};
+
+const pwaInfo = {"webManifest":{"href":"/bio/manifest.webmanifest"}};
+
+const $$FloatingHeader = createComponent(($$result, $$props, $$slots) => {
+  return renderTemplate`${maybeRenderHead()}<div class="md:hidden"${addAttribute(createTransitionScope($$result, "y2opjsap"), "data-astro-transition-persist")}> <input type="checkbox" id="mobile-menu-toggle" class="peer hidden"> <label for="mobile-menu-toggle" class="fixed top-4 right-4 z-40 p-2 rounded-full bg-white/70 dark:bg-slate-800/70 backdrop-blur-sm shadow-lg cursor-pointer" aria-label="メニューを開く" tabindex="0" role="button" aria-pressed="false" aria-controls="mobile-nav"> ${renderComponent($$result, "Bars3Icon", $$Bars3, { "class": "w-6 h-6 text-slate-800 dark:text-slate-200" })} </label> <label for="mobile-menu-toggle" class="fixed inset-0 z-30 bg-black/30 backdrop-blur-sm opacity-0 pointer-events-none peer-checked:opacity-100 peer-checked:pointer-events-auto transition-opacity duration-300" aria-hidden="true"></label> <nav id="mobile-nav" class="fixed top-0 right-0 z-40 h-full w-64 bg-white dark:bg-slate-900 shadow-xl p-6 transition-transform duration-300 translate-x-full peer-checked:translate-x-0" aria-hidden="true"> <div class="flex justify-between items-center mb-8"> <span class="text-xl font-bold">Menu</span> <div class="flex flex-nowrap"> ${renderComponent($$result, "ThemeToggle", $$ThemeToggle, {})} <button id="mobile-menu-close-button" class="p-2 rounded-full hover:bg-slate-200 dark:hover:bg-slate-700" aria-label="閉じる"> ${renderComponent($$result, "XMarkIcon", $$XMark, { "class": "w-6 h-6 text-slate-700 dark:text-slate-300" })} </button> </div> </div> ${renderComponent($$result, "NavLinks", $$NavLinks, { "class": "space-y-4 list-none", "itemClass": "block text-xl font-medium text-slate-700 dark:text-slate-300 hover:text-primary-600 dark:hover:text-primary-400" })} </nav> </div> <header id="floating-header" class="hidden md:block fixed top-0 left-0 right-0 z-20 py-3 px-8 bg-white/70 dark:bg-slate-900/70 backdrop-blur-md shadow-sm transition-transform duration-300 -translate-y-full" role="banner"> <nav class="max-w-5xl mx-auto flex justify-between items-center"> ${renderComponent($$result, "Logo", $$Logo, { "id": "floating-logo", "class": "text-xl" })} <div class="flex items-center space-x-6"> ${renderComponent($$result, "NavLinks", $$NavLinks, { "class": "flex space-x-6 list-none", "itemClass": "text-base font-medium text-slate-700 dark:text-slate-300 hover:text-primary-600 dark:hover:text-primary-400 transition-colors" })} ${renderComponent($$result, "ThemeToggle", $$ThemeToggle, {})} </div> </nav> </header> ${renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/components/FloatingHeader.astro?astro&type=script&index=0&lang.ts")} ${renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/components/FloatingHeader.astro?astro&type=script&index=1&lang.ts")}`;
+}, "C:/Users/snows/Documents/Program/js/bio/src/components/FloatingHeader.astro", "self");
+
+var __freeze = Object.freeze;
+var __defProp = Object.defineProperty;
+var __template = (cooked, raw) => __freeze(__defProp(cooked, "raw", { value: __freeze(cooked.slice()) }));
+var _a;
+const $$Astro = createAstro("https://hi2ma-bu4.github.io/");
+const $$Layout = createComponent(($$result, $$props, $$slots) => {
+  const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
+  Astro2.self = $$Layout;
+  const linkArr = [];
+  const mediaLight = "(prefers-color-scheme: light)";
+  const mediaDark = "(prefers-color-scheme: dark)";
+  pwaAssetsHead.links.map((link) => ({ ...link })).forEach((link) => {
+    linkArr.push(link);
+    if (link.rel === "icon") {
+      let newLink;
+      switch (link.href.split(".").pop()) {
+        case "ico":
+          link.media = mediaLight;
+          newLink = { ...link };
+          newLink.href = `${BASE_DIR}favicon-dark.ico`;
+          break;
+        case "svg":
+          link.media = mediaLight;
+          newLink = { ...link };
+          newLink.href = `${BASE_DIR}favicon-dark.svg`;
+          break;
+      }
+      if (newLink) {
+        newLink.media = mediaDark;
+        linkArr.push(newLink);
+      }
+    }
+  });
+  linkArr.push({ rel: "sitemap", href: `${BASE_DIR}sitemap-index.xml` });
+  if (pwaInfo) {
+    linkArr.push({ rel: "manifest", href: pwaInfo.webManifest.href });
+  }
+  const metaArr = [
+    { name: "viewport", content: "width=device-width, initial-scale=1.0" },
+    { name: "format-detection", content: "telephone=no, address=no, email=no" },
+    { name: "author", content: AUTHOR },
+    { name: "generator", content: Astro2.generator },
+    { name: "mobile-web-app-capable", content: "yes" },
+    { name: "apple-mobile-web-app-capable", content: "yes" },
+    { name: "apple-mobile-web-app-status-bar-style", content: "black-translucent" },
+    { name: "apple-mobile-web-app-title", content: SITE_TITLE }
+  ];
+  if (pwaAssetsHead.themeColor) {
+    metaArr.push({ name: "theme-color", content: pwaAssetsHead.themeColor.content });
+  }
+  const { title, description = SITE_DESCRIPTION } = Astro2.props;
+  return renderTemplate(_a || (_a = __template(['<html lang="ja" prefix="og: https://ogp.me/ns#" data-overlayscrollbars-initialize> <head prefix="website: https://ogp.me/ns/website#">', "", "", "", "", "<script async", "><\/script>", '<link rel="preconnect" href="https://fonts.googleapis.com">', "", '</head> <body class="min-h-screen flex flex-col w-full" data-overlayscrollbars-initialize> <!-- Google Tag Manager (noscript) --> <noscript><iframe', ' height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> <!-- End Google Tag Manager (noscript) --> ', " ", ' <div id="bg-canvas"', '></div> <main class="grow w-full max-w-5xl mx-auto px-4 py-8 md:px-8" role="main"> ', " </main> ", " ", " ", " ", " ", " ", " </body> </html>"])), renderComponent($$result, "SEO", $$SEO, { "charset": "UTF-8", "title": title, "titleDefault": "Page", "titleTemplate": `%s | ${SITE_TITLE}`, "description": description, "openGraph": {
+    basic: {
+      title,
+      type: "website",
+      image: BASE_DIR + "maskable-icon-512x512.png"
+    },
+    optional: {
+      description,
+      locale: "ja_JP",
+      siteName: SITE_TITLE
+    }
+  }, "twitter": { creator: "@hi2ma_bu4", card: "summary" }, "extend": {
+    link: linkArr,
+    meta: metaArr
+  } }), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=0&lang.ts"), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=1&lang.ts"), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=2&lang.ts"), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=3&lang.ts"), addAttribute(`${BASE_DIR}assets/js/static/auto-gtag.js`, "src"), renderComponent($$result, "ClientRouter", $$ClientRouter, {}), renderComponent($$result, "GoogleFontsOptimizer", $$GoogleFontsOptimizer, { "url": "https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap" }), renderHead(), addAttribute(`https://www.googletagmanager.com/ns.html?id=${GTAG_ID}`, "src"), renderComponent($$result, "Header", $$Header, {}), renderComponent($$result, "FloatingHeader", $$FloatingHeader, {}), addAttribute(createTransitionScope($$result, "hvl3seze"), "data-astro-transition-persist"), renderSlot($$result, $$slots["default"]), renderComponent($$result, "Footer", $$Footer, {}), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=4&lang.ts"), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=5&lang.ts"), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=6&lang.ts"), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=7&lang.ts"), renderScript($$result, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro?astro&type=script&index=8&lang.ts"));
+}, "C:/Users/snows/Documents/Program/js/bio/src/layouts/Layout.astro", "self");
+
+export { $$Layout as $ };
+//# sourceMappingURL=Layout-DcCVXUd6.js.map
---
+++ src/data/works.ts
@@ -0,0 +1,116 @@
+import { BASE_DIR } from "../config";
+import type { WorkItemData } from "../types/work-item";
+
+import imageKanjiSearch from "../imgs/kanjiSearch.png";
+import imageMandelbrot from "../imgs/mandelbrot.png";
+import imageMidiFallingBar from "../imgs/midiFallingBar.png";
+import imageTool2048 from "../imgs/tool2048.png";
+
+/** 作品データの一覧 */
+export const worksData: WorkItemData[] = [
+	{
+		id: "RepoShowcase",
+		title: "RepoShowcase",
+		description: "作成した個人利用用途のライブラリ一覧。",
+		imageUrl: "",
+		tags: ["Hub Page"],
+		directLink: `https://hi2ma-bu4.github.io/RepoShowcase/`,
+		githubLink: "https://github.com/hi2ma-bu4/RepoShowcase",
+		longDescription: `作成したライブラリ(ほとんど個人利用用途)の一覧とそのデモページを設置しています。`,
+	},
+	{
+		id: "KanjiSearch",
+		title: "漢字手書き検索ツール",
+		description: "オフラインで漢字を手書き検索できるツールです。",
+		imageUrl: imageKanjiSearch,
+		directLink: `${BASE_DIR}KanjiSearch/`,
+		githubLink: "https://github.com/hi2ma-bu4/KanjiSearch",
+		tags: ["JavaScript", "TypeScript", "WebAssembly", "Python", "ONNX-Runtime"],
+		longDescription: `このツールは、オフラインで漢字を手書き検索できる機能を提供します。
+(初回起動時にモデルのダウンロードが必要です)
+手書きの漢字を認識するために、ONNX形式の機械学習モデルを使用しています。
+精度がかなり悪いため、あまり実用的ではありませんが、onnxの勉強で作成しました。`,
+	},
+	{
+		id: "MIDI-FallingBar",
+		title: "MIDI FallingBar",
+		description: "MidiをWebでYouTubeで見るような形式で再生できます。",
+		imageUrl: imageMidiFallingBar,
+		tags: ["JavaScript", "TypeScript", "GLSL", "Three.js", "Tone.js"],
+		directLink: `${BASE_DIR}MIDI-FallingBar/`,
+		githubLink: "https://github.com/hi2ma-bu4/MIDI-FallingBar",
+		longDescription: `「Midiをグラフィカルに再生したい」という突然の発想により、生み出された産物です。
+これは、現代の自動演奏ピアノやSynthesiaスタイルの動画にインスパイアされた、MIDIノートを3Dピアノロール形式の「落下するバー」として表示するウェブベースのMIDIビジュアライザーです。
+WebGLレンダリングにはThree.jsを、MIDIファイルのパースには@tonejs/midiを使用しています。`,
+	},
+	{
+		id: "tool-2048",
+		title: "Tool 2048",
+		description: "2048ゲームの最適解計算ツールです。",
+		imageUrl: imageTool2048,
+		directLink: `${BASE_DIR}tool-2048/`,
+		githubLink: "https://github.com/hi2ma-bu4/tool-2048",
+		tags: ["JavaScript", "TypeScript", "WebAssembly"],
+		longDescription: `このツールは、パズルゲーム「2048」における最適解を計算するために設計されています。
+盤面の状態を分析し、効率的な手順を探索することで、
+最小手数での到達や高スコア戦略の検証に活用できます。`,
+	},
+	{
+		id: "ReTrans",
+		title: "ReTrans",
+		description: "再翻訳ブームに乗ったページです。",
+		imageUrl: "",
+		directLink: `${BASE_DIR}ReTrans/`,
+		githubLink: "https://github.com/hi2ma-bu4/ReTrans",
+		tags: ["JavaScript", "GAS"],
+		longDescription: `巷で噂の「再翻訳」で遊べるページです。
+
+以下のURLで、カスタム再翻訳が試す事ができます。`,
+		otherUrls: [`${BASE_DIR}ReTrans/?ja&en&gd&az&lo&ur&yo&ko&ar&is&gd&lb&ur&yi&vi&be&de&rw&sl&ny&zh-CN&ja`],
+	},
+	{
+		id: "mandelbrot",
+		title: "Mandelbrot Set",
+		description: "マンデルブロ集合の表示ページです。",
+		imageUrl: imageMandelbrot,
+		directLink: `${BASE_DIR}mandelbrot/`,
+		githubLink: "https://github.com/hi2ma-bu4/mandelbrot",
+		tags: ["JavaScript", "GLSL", "PC Only"],
+		longDescription: `このページでは、マンデルブロ集合（Mandelbrot Set）を視覚的に探索できます。
+複雑な数値計算に基づき、美しいフラクタル模様を描画し、
+ズームを通して、数学的構造の無限の奥行きを体験できます。
+
+OpenGL Shading Language의数値上限によりズーム限界が決まっています。`,
+	},
+	{
+		id: "js-ryoisyou",
+		title: "Ryo is You",
+		description: "Baba is Youのパクリです。",
+		imageUrl: "",
+		directLink: `${BASE_DIR}ryoisyou/`,
+		githubLink: "https://github.com/hi2ma-bu4/ryoisyou",
+		tags: ["JavaScript", "PC Only"],
+		longDescription: `本ページの内容は「Baba Is You」に着想を得て構成されています。
+つまりパクリということです。`,
+	},
+	{
+		id: "js-minecraft",
+		title: "js-minecraft",
+		description: "DOMだけでMinecraftを再現したかった...",
+		imageUrl: "",
+		directLink: `${BASE_DIR}js-minecraft/`,
+		githubLink: "https://github.com/hi2ma-bu4/js-minecraft",
+		tags: ["JavaScript", "PC Only"],
+		longDescription: `DOMだけでMinecraftを再現しているページがあり、
+そのページを9割真似て作成したものです。`,
+	},
+];
+
+/** 作品IDをキーとしたマップデータ */
+export const workItemsMap = worksData.reduce(
+	(acc, work) => {
+		acc[work.id] = work;
+		return acc;
+	},
+	{} as Record<string, WorkItemData>,
+);
